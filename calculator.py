563221f2b1078fdc2c64aa72f2fce4b8d87ddfb6
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-01 13:55:58 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
+#Erik Hansen
+#11/1/2017
+#calculator.py - program to make calculator applet
 
+from ggame import *
+
+black = black = Color(0x000000,1)
+blackoutline = LineStyle(1,black)
+white = Color(0xFFFFFF,1)
+
+calcoutline = RectangleAsset(300,350,blackoutline,white)
+answerbar = RectangleAsset(275,50,blackoutline,white)
+keys = RectangleAsset(50,50,blackoutline,white)
+Sprite(calcoutline)
+Sprite(answerbar, (12.5,12.5))
+Sprite(keys, (12.5,75))
+
+App().run()
