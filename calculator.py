64f2c242b4a2a8fcdb7ca103a2ae9554d6ab8f64
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-02 14:24:23 -0400

    Updated from Brython Server: 11/2/2017 2:24:21 PM

--- calculator.py
+++ calculator.py
@@ -6,16 +6,20 @@ from ggame import *
 
 black = black = Color(0x000000,1)
 blackoutline = LineStyle(1,black)
-white = Color(0xFFFFFF,1)
+white = Color(0xFFFFFF,0)
+red = Color(0xFF0000,.5)
 
 calcoutline = RectangleAsset(300,350,blackoutline,white)
 answerbar = RectangleAsset(275,50,blackoutline,white)
-keys = RectangleAsset(50,50,blackoutline,white)
+keys = RectangleAsset(55,55,blackoutline,white)
+equalkey = RectangleAsset(55,55,blackoutline,red)
 Sprite(calcoutline)
 Sprite(answerbar, (12.5,12.5))
 for j in range(4):
-    for i in range(3):
-        Sprite(keys,(12.5 + 62.5*i, 75 + 62.5*j))
+    for i in range(4):
+        Sprite(keys,(12.5 + 72.5*i, 80 + 68*j))
+        if i == 3 and j == 3:
+            Sprite(equalkey, (230,284))
 
 
 App().run()
